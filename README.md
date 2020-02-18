96dae4bacd4bf75b9069aad9e5e1c3c13a47e40f
Aaron Hunt <anhuntz@gmail.com>
2020-02-18 01:53:24 -0500

    Finished Batch

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # civil-server-tools
 Simple Tools for Civil Community Servers | https://civil-game.com/
+
+# Getting Started
+Before doing anything, ensure you have the `civilServer.exe` tool installed and up to date from Steam.
+
+Next, `civ-server.bat` is the primary tool for running a Civil Server. Before launching, ensure you set up your server location in the batch file. This is so you can package and run the server with your custom parameters. 
+
+Things to keep in mind:
+
+- Port 7777 must be forwarded; The Unreal Engine Default Port.
+- Server icon should be 32px.
+- You can set custom server parameters in the batch file.
+- Plugin Support is currently unavailable.
+- PlayerData is transferred but unfunctional in this build.
+
+`Civil Development Kit - Server Tools 0.1`
--- civ-server.bat
+++ civ-server.bat
@@ -2,32 +2,62 @@
 :: Ensure the directory below is correct before launching!
 @echo off
 SET location=G:\civilthegame-master\compile\WindowsServer
+:: Erase "-log" to run server in background.
+SET params=-log
 
 :: Check Directory
 
 if exist %location%\civilServer.exe (
 
 :: Begin Config Deployment
-echo Deploying Config
+echo ====Packaging Config=====
+
+@echo on
 
 cd ServerConfig
-
+break>Config.civ
 for /f "tokens=1,2 delims==" %%a in (server-info.ini) do (
 SETLOCAL EnableDelayedExpansion
 if %%a==NAME set NAME=%%b
 if %%a==DESCRIPTION set DESCRIPTION=%%b
-    echo(!NAME!!DESCRIPTION!
+if %%a==ICON set ICON=%%b
+if %%a==MAX_PLAYERS set MAX_PLAYERS=%%b
+if %%a==WORLD set WORLD=%%b
+if %%a==GAMEMODE set GAMEMODE=%%b
+    echo(!NAME!!DESCRIPTION!!ICON!!MAX_PLAYERS!!WORLD!!GAMEMODE!
     ENDLOCAL
-)
+)>>Config.civ
+
+for /f "tokens=1,2 delims==" %%a in (preferences.ini) do (
+SETLOCAL EnableDelayedExpansion
+if %%a==OUTPUT_SERVER_LOG set OUTPUT_SERVER_LOG=%%b
+if %%a==GLOBAL_GRAVITY set GLOBAL_GRAVITY=%%b
+    echo(!OUTPUT_SERVER_LOG!!GLOBAL_GRAVITY!
+    ENDLOCAL
+)>>Config.civ
+
+@echo off
 
 
 
 :: Packaging Config Files
-echo Packaging Config
-cd..
-pause
+echo ======Deploying Config======
+@echo on
+:: xcopy /s /y ..\Plugins\*.* %location%\civil\Content\Paks
+move Config.civ %location%
+xcopy /s /y ..\PlayerData\*.* %location%
 
+echo ............................
+
+
+:: Running Dedicated Server
+echo ======Starting Server======
+start %location%\civilServer.exe %params%
+@echo off
 ) else (
-echo Directory invalid.
+echo Error: Directory invalid.
 pause
 )
+
+
+:: Window should close without errors.
